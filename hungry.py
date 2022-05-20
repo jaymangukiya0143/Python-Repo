25144ca26ea824197b4216acfd093d8cf53f50aa
Jay Mangukiya <jaymangukiya0143@gmail.com>
2022-05-20 15:11:59 +0530

    fries removed

--- hungry.py
+++ hungry.py
@@ -2,12 +2,8 @@ hungry = input("Are you thirsty?")
 
 if hungry == "yes":
     print("Eat samosa")
-<<<<<<< HEAD
     print("Eat pizza")
     print("Eat burger")
-    print("Eat Fries")
-=======
->>>>>>> thirsty
 else:
     thirsty = input("Are you thirsty")
     if thirsty == "yes":
