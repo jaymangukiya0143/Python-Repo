497a37618ccadcb83707b02ca2144159b29fa46f
Jay Mangukiya <jaymangukiya0143@gmail.com>
2022-05-20 16:12:51 +0530

    changed condition

--- hungry.py
+++ hungry.py
@@ -1,11 +1,13 @@
 hungry = input("Are you thirsty?")
 
-if hungry == "yes":
+if hungry == "yes" and hungry == "Y":
     print("Eat samosa")
     print("Eat pizza")
     print("Eat burger")
+    print("Eat Fries")
 else:
     thirsty = input("Are you thirsty")
     if thirsty == "yes":
         print("Drink Water")
         print("Drink Colddrink")
+        print("Drink Beer")
